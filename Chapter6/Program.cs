866ee33ab18ecf8f65d5bc4b60c3be388aac3c59
alsals126 <9970mia@naver.com>
2021-08-23 16:09:38 +0900

    쉐도잉, 하이딩, 오버라이딩, 추상 클래스, 추상 메서드 예제

--- Chapter6/Program.cs
+++ Chapter6/Program.cs
@@ -24,8 +24,70 @@ namespace Chapter6
     }
     class Program
     {
+        abstract class AbstractParent
+        {
+            public abstract void Test();
+        }
+        class AbstractChild : AbstractParent
+        {
+            public override void Test() { } // new하면 안되고, override 키워드를 넣어야한다.
+        }
+
+        class Parent
+        {
+            public int variable = 273;
+            public void Method()
+            {
+                Console.WriteLine("부모의 메서드");
+            }
+
+            // [자바와 큰 차이]
+            // 자식에서 오버라이드를 할 때에는(오버라이드를 당할 때) virtual이라는 키워드를 붙여준다.
+            /*
+            public virtual void Method()
+            {
+                Console.WriteLine("부모의 메서드");
+            }
+            */
+        }
+        class Child : Parent
+        {
+            public new string variable = "shadowing"; // new를 쓰면 안쓸때와 달리 초록색밑줄이 안나타난다. ('하이딩'하겠다고 선언)
+            public new void Method() //하이딩 => C#에서는 오버라이딩할때는 표시를 한다
+            {
+                Console.WriteLine("자식의 메서드");
+            }
+            
+            // [자바와 큰 차이]
+            // 오버라이드 할 때 override라는 키워드를 붙인다. => 오버라이드하고 싶은 것만 허용한다는 장점? (부분적으로 허용)
+            /*
+            public override void Method() //하이딩 => C#에서는 오버라이딩할때는 표시를 한다
+            {
+                Console.WriteLine("자식의 메서드");
+            }
+            */
+        }
         static void Main(string[] args)
         {
+            Child child = new Child();
+            child.Method();
+            ((Parent)child).Method();
+
+            //Console.WriteLine((new Child()).variable);
+            //Console.WriteLine(((Parent)(new Child())).variable);
+            /*
+            List<Animal> Animals = new List<Animal>()
+            {
+                new Dog(), new Cat(), new Cat(), new Dog(),
+                new Dog(), new Cat(), new Cat(), new Dog()
+            };
+            foreach(var item in Animals)
+            {
+                item.Eat();
+                item.Sleep();
+                ((Cat)item).Meow();
+            }
+            */
         }
     }
 }
