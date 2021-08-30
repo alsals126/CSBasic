6601f937bb27530eab589a76165bf3ba869c3af9
alsals126 <9970mia@naver.com>
2021-08-30 14:46:49 +0900

    Generic 에제

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace CSBasic7
 {
+    class Wanted<T>
+        //where T : Student //Student클래스만 사용할 수 있게 함
+    {
+        public T Value;
+        public Wanted(T value)
+        {
+            this.Value = value;
+        }
+    }
+
+    class Student { }
     class Program
     {
         static void Main(string[] args)
         {
+            Wanted<string> wantedString = new Wanted<string>("스트링");
+            Wanted<int> wantedInt = new Wanted<int>(3);
+            Wanted<Student> wantedStudent = new Wanted<Student>(new Student());
         }
     }
 }
