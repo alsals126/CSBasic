b7ecddd436c432025d185957193ee2c67fbe195e
alsals126 <9970mia@naver.com>
2021-08-30 15:49:02 +0900

    구조체와 구조체 생성자, 객체와의 차이 예제

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -38,6 +38,37 @@ namespace CSBasic7
     class Student { }
     class Program
     {
+        class PointClass
+        {
+            public int x;
+            public int y;
+            public PointClass(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        struct Point
+        {
+            public int x;
+            public int y;
+            public string testA;
+            //public string testB = "init";
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = "";
+            }
+            public Point(int x, int y, string a)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = a;
+            }
+        }
+
         static void NextPosition(int x, int y, int vx, int vy, out int rx, out int ry)
         {
             rx = x + vx;
@@ -45,6 +76,29 @@ namespace CSBasic7
         }
         static void Main(string[] args)
         {
+            PointClass pcA = new PointClass(10, 20); //new 키워드를 할 때 공간이 잡히게 된다.
+            PointClass pcB = pcA; //pcA가 가리키고 있는 주소값을 가지게 된다.
+            pcB.x = 100;
+            pcB.y = 200; 
+            Console.WriteLine(pcA.x + "/" + pcA.y); // 100/200
+            Console.WriteLine(pcB.x + "/" + pcB.y); // 100/200
+            Console.WriteLine();
+
+            Point pA = new Point(10, 20); //Point pA를 하는 순간(변수를 선언할 때) 공간이 잡히게 된다.
+            Point pB = pA; // pA의 값을 가지게 된다.
+            pB.x = 100;
+            pB.y = 200;
+            Console.WriteLine(pA.x + "/" + pA.y); // 10/20
+            Console.WriteLine(pB.x + "/" + pB.y); // 100/200
+            Console.WriteLine();
+
+            Point point = new Point(10, 10);
+            Point point2 = new Point();
+            //point.x = 10;
+            //point.y = 10;
+            Console.WriteLine(point2.x); //위에서 초기화안하면 오류가 난다.
+            Console.WriteLine(point2.y);
+
             int x = 0;
             int y = 0;
             int vx = 100;
@@ -53,7 +107,7 @@ namespace CSBasic7
             NextPosition(x, y, vx, vy, out x, out y);
             Console.WriteLine("다음 좌표: (" + x + "," + y + ")");
 
-            Console.WriteLine("숫자 입력: ");
+            Console.Write("숫자 입력: ");
             int output = int.MinValue; //사용자가 적지 않을 숫자로 초기화한다.
             bool result = int.TryParse(Console.ReadLine(), out output);
             if (result)
