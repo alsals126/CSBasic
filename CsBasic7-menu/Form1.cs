00fbcb39782a50a70854d9635850ee307a8e8d18
alsals126 <9970mia@naver.com>
2021-08-30 16:09:32 +0900

    윈폼-MenuStrip, StatusStrip

--- CsBasic7-menu/Form1.cs
+++ CsBasic7-menu/Form1.cs
@@ -16,5 +16,42 @@ namespace CsBasic7_menu
         {
             InitializeComponent();
         }
+
+        private void 파일FToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 다른이름으로저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 도움ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 메모장정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("가짜 메모장~");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (toolStripProgressBar1.Value + 10 < toolStripProgressBar1.Maximum)
+                toolStripProgressBar1.Value += 10;
+            else
+                toolStripProgressBar1.Value = toolStripProgressBar1.Maximum;
+        }
+
+        private void 상태표시줄ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            상태표시줄ToolStripMenuItem.Checked = !상태표시줄ToolStripMenuItem.Checked;
+            if (상태표시줄ToolStripMenuItem.Checked)
+                statusStrip1.Visible = true;
+            else
+                statusStrip1.Visible = false;
+        }
     }
 }
